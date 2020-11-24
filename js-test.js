2b3bbec514a8a99749f8c739ee4fcc618d1f8641
Sylvie B <unsolo@hotmail.fr>
2020-11-24 13:43:18 +0100

    avec deux essais

--- js-test.js
+++ js-test.js
@@ -1,18 +1,38 @@
+/* demande le chiffre secret */
+let secret  = prompt("entrer le chiffre à deviner")
+
+/* demande le nom du joueur */
 let name = prompt("Quel est votre pseudo ?");
 
 alert("Bonjour, " + name);
 
+/* demande un chiffre entre 1 et 10 */
 let number = prompt("Entre un chiffre de 1 à 10");
+console.log(number)
 
-if(number > 5) {
-    alert("essai avec un chiffre plus petit");
+/* compare les 2 chiffres */
+if( number === secret){
+    alert("Bravo, " + name + ", tu as gagné !");
 }
 
-else if(number < 5) {
-    alert("essai avec un chiffre plus grand")
+
+else if(number > secret) {
+    let numDeux = prompt("essai avec un chiffre plus petit");
+
+    if (numDeux === secret){
+        alert("Bravo, " + name + ", tu as gagné !");
+    }
+
+    else{
+        alert("Désolé, " + name + ", tu as perdu :(")
+    }
 }
 
-else{
-    alert("Bravo, " + name + ", tu as gagné !")
+else {
+    let numTrois = prompt("essai avec un chiffre plus grand");
+
+    if (numTrois === secret){
+        alert("Bravo, " + name + ", tu as gagné !");
+    }
 }
 
