e63b8b2bb343e2034d0ad647af4fcd99f89e1171
Sylvie B <unsolo@hotmail.fr>
2020-11-24 10:34:37 +0100

    demande un nombre et le compare

---
+++ js-test.js
@@ -0,0 +1,21 @@
+let name = prompt("Quel est votre pseudo ?");
+
+alert("Bonjour, " + name);
+
+let number = prompt("Entre un chiffre de 1 à 10");
+
+if(number == 5) {
+    alert("Bravo ! " + name + ", tu as gagné !");
+}
+
+else if(number < 5) {
+    alert("le chiffre est plus grand")
+}
+
+else  if( number > 5) {
+    alert("le chiffre est plus petit");
+}
+
+
+
+
